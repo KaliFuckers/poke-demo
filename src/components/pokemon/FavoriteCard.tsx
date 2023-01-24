719a58edae9a43159478334517e8996867f751bc
shammael <shammamnd2015@gmail.com>
2023-01-24 13:15:40 -0400

    refactor: :art: Create FavoriteCard Component

---
+++ src/components/pokemon/FavoriteCard.tsx
@@ -0,0 +1,41 @@
+import { PokemonData } from '@/interfaces';
+import { pokeApi } from '@/services';
+import Link from 'next/link';
+import { useEffect, useState } from 'react';
+import { Card, Skeleton } from '../ui';
+
+interface Props {
+  id: Number;
+}
+
+export default function FavoriteCard({ id }: Props) {
+  const [pokemon, setPokemon] = useState<PokemonData | null>(null);
+
+  useEffect(() => {
+    async function getPoke() {
+      const poke = await pokeApi.get<PokemonData>(`/pokemon/${id}`);
+      setPokemon(poke.data);
+    }
+
+    getPoke();
+  }, [id]);
+
+  return pokemon ? (
+    <Link href={`/pokemon/${pokemon?.name}`}>
+      <Card className="w-full h-full ">
+        <Card.Image
+          src={
+            pokemon
+              ? `https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/other/dream-world/${pokemon.id}.svg`
+              : '/img/no-image.png'
+          }
+          alt={`poke ${pokemon?.name}`}
+          fill
+          priority
+        />
+      </Card>
+    </Link>
+  ) : (
+    <Skeleton />
+  );
+}
--- src/components/pokemon/Favorites.tsx
+++ src/components/pokemon/Favorites.tsx
@@ -1,5 +1,4 @@
-import Link from 'next/link';
-import { Card } from '../ui';
+import FavoriteCard from './FavoriteCard';
 
 interface Props {
   favorites: Number[];
@@ -9,15 +8,7 @@ export default function Favorites({ favorites }: Props) {
   return (
     <div className="grid grid-cols-2 md:grid-cols-3 lg:grid-cols-4 gap-4 auto-rows-[minmax(150px,_auto)]">
       {favorites.map((fav) => (
-        <Link href={`/pokemon/${fav}`} key={fav.toString()}>
-          <Card className="w-full h-full ">
-            <Card.Image
-              src={`https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/other/dream-world/${fav}.svg`}
-              alt={`poke ${fav}`}
-              fill
-            />
-          </Card>
-        </Link>
+        <FavoriteCard id={fav} key={fav.toString()} />
       ))}
     </div>
   );
