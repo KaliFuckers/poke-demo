cb8733e5f4306bf66b3f00ffdc5046acecd39688
shammael <shammamnd2015@gmail.com>
2023-01-24 13:45:45 -0400

    style: Export default FavoriteCard component

--- src/components/pokemon/index.ts
+++ src/components/pokemon/index.ts
@@ -1,3 +1,4 @@
 export { default as PokemonCard } from './card';
 export { default as NoFavorites } from './NoFavorites';
 export { default as Favorites } from './Favorites';
+export { default as FavoriteCard } from './FavoriteCard';
