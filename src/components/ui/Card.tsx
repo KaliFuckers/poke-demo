196c16b782bc05af764ea762cd073f1a967c0169
shammael <shammamnd2015@gmail.com>
2023-01-25 14:45:15 -0400

    style: :lipstick: Update the Card to handle various effect

--- src/components/ui/Card.tsx
+++ src/components/ui/Card.tsx
@@ -1,29 +1,122 @@
 import NextImage, { ImageProps } from 'next/image';
-import { HTMLAttributes, PropsWithChildren } from 'react';
+import {
+  HTMLAttributes,
+  PropsWithChildren,
+  useEffect,
+  useRef,
+  useState,
+} from 'react';
 
 interface Props {
   className?: string;
 }
 
-function Card(props: PropsWithChildren<HTMLAttributes<HTMLElement>>) {
-  const { className, children } = props;
+function Card(
+  props: PropsWithChildren<
+    HTMLAttributes<HTMLElement> & {
+      hoverable?: boolean;
+      effect?: boolean;
+      rippleEffect?: boolean;
+    }
+  >,
+) {
+  const [coords, setcoords] = useState({ x: -1, y: -1 });
+  const [isRipple, setisRipple] = useState(false);
+
+  useEffect(() => {
+    if (coords.x !== -1 && coords.y !== -1) {
+      setisRipple(true);
+      setTimeout(() => setisRipple(false), 500);
+    } else {
+      setisRipple(false);
+    }
+  }, [coords]);
+
+  useEffect(() => {
+    if (!isRipple) {
+      setcoords({ x: -1, y: -1 });
+    }
+  }, [isRipple]);
+
+  const containerRef = useRef<HTMLInputElement>(null);
+
+  const handleClick = (e: any) => {
+    // const size = containerRef.current!.offsetWidth;
+    const pos = containerRef.current!.getBoundingClientRect();
+
+    setcoords({
+      x: e.clientX - pos.left,
+      y: e.clientY - pos.top,
+    });
+  };
+
+  const removeProp1 = 'hoverable';
+  const removeProp2 = 'className';
+  const removeProp3 = 'effect';
+  const removeProp4 = 'children';
+  const removeProp5 = 'rippleEffect';
+  const removeProp6 = 'onClick';
+  const {
+    [removeProp1]: remove,
+    [removeProp2]: remove2,
+    [removeProp3]: remove3,
+    [removeProp4]: remove4,
+    [removeProp5]: remove5,
+    [removeProp6]: remove6,
+    ...renderingProps
+  } = props;
+  const {
+    className,
+    hoverable,
+    effect,
+    onClick: propsClick,
+    children,
+    rippleEffect = false,
+  } = props;
+
   return (
     <div
-      {...props}
-      className={`drop-shadow-md hover:drop-shadow-lg bg-white bg-opacity-5 p-4 rounded-md ${className}`}
+      onClick={(e) => {
+        handleClick(e);
+        if (propsClick) {
+          propsClick(e);
+        }
+      }}
+      onKeyDown={(e: any) => {
+        handleClick(e);
+        if (propsClick) {
+          propsClick(e);
+        }
+      }}
+      role="button"
+      tabIndex={0}
+      style={{
+        overflow: 'hidden',
+      }}
+      ref={containerRef}
+      {...renderingProps}
+      className={`drop-shadow-md hover:drop-shadow-lg bg-white bg-opacity-5 p-4 rounded-md ${
+        effect && 'hover:mt-[-5px] transition-all duration-[350ms]'
+      } ${hoverable ? 'cursor-pointer' : 'cursor-default'} ${className}`}
     >
-      {children}
+      {isRipple && rippleEffect && (
+        <span
+          style={{ top: coords.y, left: coords.x }}
+          className="w-5 h-5 absolute bg-white rounded-full translate-x-[-50%] translate-y-[-50%] animate-ripple-effect"
+        />
+      )}
+      <div className="h-full w-full relative z-20 flex flex-col">
+        {children}
+      </div>
     </div>
   );
 }
 
-// eslint-disable-next-line react/require-default-props
 function Image(props: ImageProps & { full?: boolean }) {
   const removeProp = 'full';
   const { [removeProp]: remove, full, ...renderingProps } = props;
   return (
     <div className="relative w-full h-full">
-      {}
       <NextImage {...renderingProps} />
     </div>
   );
